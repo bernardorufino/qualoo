6999566ec1778b711a3c6547e821b668209f5059
Bernardo Rufino <bermonruf@gmail.com>
2012-01-18 04:45:59 -0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qualoo::Application.config.secret_token = 'fac863c67537529e4f434d1aaa3f48dba36d73ac15606d56d5ce9b89fbb538ea04d3952dcba4994512226c614421797089a102b1acbaf7842fb0954a07d23337'
